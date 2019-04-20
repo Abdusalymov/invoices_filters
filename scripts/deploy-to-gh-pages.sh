4e2680c5b8edbdbb195516531ab6247e4a8bbcc4
Abdusalymov <irbis180288@yandex.ru>
2019-04-20 18:06:24 +0500

    change ..pages.sh

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -1,82 +1,63 @@
-# #!/bin/bash
+#!/bin/bash
 
-# # Exit with nonzero exit code if anything fails.
-# set -e
+# Exit with nonzero exit code if anything fails.
+set -e
 
-# # Define repository relative GitHub address.
-# repositoryRelativeGitHubAddress="Flexberry/ember-flexberry"
+echo "Pull request: ${TRAVIS_PULL_REQUEST}"
+echo "Parameter: ${1}"
 
-# # Clone project into 'repository' subdirectory && move to it.
-# echo "Prepare for deploy to gh-pages."
-# echo "Clone ${repositoryRelativeGitHubAddress} repository & checkout latest version of gh-pages branch."
-# git clone --recursive "https://github.com/${repositoryRelativeGitHubAddress}.git" emberFlexberryRepository
-# cd emberFlexberryRepository
+if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]
+then
+  echo "Not PR"
+  if [[ "${1}" == "master" ]] || [[ "${1}" == "develop" ]]
+  then
+    echo "Correct branch"
+    # Define repository relative GitHub address.
+    repositoryRelativeGitHubAddress="Abdusalymov/invoices_filters"
 
-# # Checkout gh-pages branch & pull it's latest version.
-# git checkout gh-pages
-# git pull
+    # Clone project into 'repository' subdirectory && move to it.
+    echo "Prepare for deploy to gh-pages."
+    echo $PWD
+    echo "Clone ${repositoryRelativeGitHubAddress} repository & checkout latest version of gh-pages branch."
+    git clone --recursive "https://github.com/${repositoryRelativeGitHubAddress}.git" repo
+    cd repo
+    echo $PWD
 
-# # Navigate into dummy deploy directory.
-# cd dummy
+    # Checkout gh-pages branch & pull it's latest version.
+    git checkout gh-pages
+    git pull
 
-# # Remove results of previous deploy (for current branch) & recreate directory.
-# echo "Remove results of previous deploy (for ${TRAVIS_BRANCH} branch)."
-# rm -rf "${TRAVIS_BRANCH}"
-# mkdir "${TRAVIS_BRANCH}"
+    # Remove results of previous deploy (for current branch) & recreate directory.
+    echo "Remove results of previous deploy (for ${TRAVIS_BRANCH} branch)."
+    rm -rf "${TRAVIS_BRANCH}"
+    mkdir "${TRAVIS_BRANCH}"
 
-# # Copy builded ember application from 'dist' directory into 'repository/${TRAVIS_BRANCH}'.
-# echo "Copy builded ember application (for ${TRAVIS_BRANCH} branch)."
-# cp -r ../../dist/* "${TRAVIS_BRANCH}"
+    # Copy builded ember application from 'dist' directory into 'repository/${TRAVIS_BRANCH}'.
+    echo "Copy application (for ${TRAVIS_BRANCH} branch)."
+    cp -r ../src/* "${TRAVIS_BRANCH}"
+    cd "${TRAVIS_BRANCH}"
+    ls -l
+    cd ..
 
-# if [[ -z "$1" || ${1} != "no-doc" ]]
-# then
-#   cd ../..
+    # Prevent "empty" commit and build failure.
+    if [ -f ".gitkeep" ]
+    then
+      rm -rf .gitkeep
+    else
+      touch .gitkeep
+    fi
 
-#   # Generate autodoc.
-#   npm install -g yuidocjs
+    # # Configure git.
+    # git config user.name "ehaberev"
+    # git config user.email "ekhaberev@ics.perm.ru"
 
-#   # Define yuidoc theme repository relative GitHub address.
-#   repositoryYuidocTheme="Flexberry/flexberry-yuidoc-theme"
+    echo "Commit & push changes."
+    git add --all
+    git commit -m "Update gh-pages for ${TRAVIS_BRANCH} branch"
 
-#   # Clone project into 'repositoryYuidocTheme' subdirectory && move to it.
-#   echo "Prepare for deploy to gh-pages."
-#   echo "Clone ${repositoryYuidocTheme} repository & checkout latest version of gh-pages branch."
-#   git clone --recursive "https://github.com/${repositoryYuidocTheme}.git" repositoryYuidocTheme
-#   cd repositoryYuidocTheme
+    # Redirect any output to /dev/null to hide any sensitive credential data that might otherwise be exposed.
+    git push --force --quiet "https://${GH_TOKEN}@github.com/${repositoryRelativeGitHubAddress}.git" > /dev/null 2>&1
+  fi
+fi
 
-#   # Checkout and pull same branch.
-#   git checkout ${TRAVIS_BRANCH}
-#   git pull
-
-#   echo "Copy ember addon source (for ${TRAVIS_BRANCH} branch) into addon directory."
-#   mkdir addon
-#   cp -r ../addon/* addon
-
-#   echo "Execute yuidoc autodocumentation generator."
-#   yuidoc
-
-#   echo "Navigate to target directory for autodoc in gh-pages."
-#   cd "../emberFlexberryRepository/autodoc"
-
-#   # Remove results of previous deploy (for current branch) & recreate directory.
-#   rm -rf "${TRAVIS_BRANCH}"
-#   mkdir "${TRAVIS_BRANCH}"
-
-#   echo "Copy autodoc result into ${TRAVIS_BRANCH} directory."
-#   cp -r ../../repositoryYuidocTheme/autodoc-result/* ${TRAVIS_BRANCH}
-# fi
-
-# cd ..
-
-# # Configure git.
-# git config user.name "Flexberry-man"
-# git config user.email "mail@flexberry.net"
-
-# echo "Commit & push changes."
-# git add --all
-# git commit -m "Update gh-pages for ${TRAVIS_BRANCH} branch"
-
-# # Redirect any output to /dev/null to hide any sensitive credential data that might otherwise be exposed.
-# git push --force --quiet "https://${GH_TOKEN}@github.com/${repositoryRelativeGitHubAddress}.git" > /dev/null 2>&1
-
-# echo "Deploy to gh-pages finished."
+echo "Deploy to gh-pages finished."
